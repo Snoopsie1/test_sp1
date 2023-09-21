8b18934a2aba9f53a05481ea13789a5eef7077f6
Snoopsie1 <oliverrasoli@gmail.com>
2023-09-21 13:13:16 +0200

    Test with Jest

--- src/test/calc.test.ts
+++ src/test/calc.test.ts
@@ -1,6 +1,6 @@
-import { add } from '../calc';
+import { add } from '../functions/calc';
 
-describe('Test Add Function:', () => {
+describe('2 (Two Katas) Test Add Function:', () => {
 	it('should return 15 for add(10,5)', () => {
 		expect(add(10, 5)).toBe(15);
 	});
@@ -8,3 +8,4 @@ describe('Test Add Function:', () => {
 		expect(add(2, 3)).toBe(5);
 	});
 });
+
