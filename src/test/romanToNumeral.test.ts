1ad981dca842a865555c1190063ca1997bed6d32
Snoopsie1 <oliverrasoli@gmail.com>
2023-09-21 13:49:15 +0200

    test for non-roman inputs

--- src/test/romanToNumeral.test.ts
+++ src/test/romanToNumeral.test.ts
@@ -4,4 +4,7 @@ describe('Test Roman-Numbers to Arabic-Numbers Function:', () => {
   it('Should return 14', () => {
     expect(romanToArabic("XIV")).toBe(14);
   });
+  it('Test to check how it reacts to non-roman inputs', () => {
+    expect(romanToArabic("foo")).toBe(NaN);
+  });
 });
